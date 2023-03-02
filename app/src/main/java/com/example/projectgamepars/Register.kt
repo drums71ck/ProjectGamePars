4f6c28c3f111fc4bde20331178f4a934e718e720
drums71ck <theyayo500@gmail.com>
2023-03-02 19:00:47 +0100

    Register and login buttons also the register window

--- app/src/main/java/com/example/projectgamepars/Register.kt
+++ app/src/main/java/com/example/projectgamepars/Register.kt
@@ -2,11 +2,20 @@ package com.example.projectgamepars
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.HashMap
 
 class Register : AppCompatActivity() {
     lateinit var correoEt :EditText
@@ -14,6 +23,7 @@ class Register : AppCompatActivity() {
     lateinit var nicknameEt: EditText
     lateinit var fechaTxt: TextView
     lateinit var Register: Button
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -29,6 +39,79 @@ class Register : AppCompatActivity() {
         val formatedDate = formatter.format(date)
 
         fechaTxt.setText(formatedDate)
+        auth = FirebaseAuth.getInstance()
+
+        Register.setOnClickListener(){
+            var email:String = correoEt.getText().toString()
+            var pass:String = passEt.getText().toString()
+
+            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                correoEt.setError("Invalid Mail")
+            }else if(pass.length<6){
+                passEt.setError("Password less than 6 characters")
+            }else{
+                RegisterPlayer(email, pass)
+            }
+
+
+
+
+        }
+
+
+
+    }
+
+
+
+    private fun RegisterPlayer(email: String, pass: String) {
+        auth.createUserWithEmailAndPassword(email, pass)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(this,"createUserWithEmail:success",Toast.LENGTH_SHORT).show()
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                        Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    //updateUI(null)
+                }
+
+            }
+
+    }
+    fun updateUI(user:FirebaseUser?){
+        if(user!=null){
+            var puntuacio: Int = 0
+            var uidString: String = user.uid
+            var correoString: String = correoEt.getText().toString()
+            var passString: String = passEt.getText().toString()
+            var nombreString: String = nicknameEt.getText().toString()
+            var fechaString: String = fechaTxt.getText().toString()
+            var dadesJugador : HashMap<String, String> = HashMap<String,String>()
+            dadesJugador.put ("Uid",uidString)
+            dadesJugador.put ("Email",correoString)
+            dadesJugador.put ("Password",passString)
+            dadesJugador.put ("Nom",nombreString)
+            dadesJugador.put ("Data",fechaString)
+            dadesJugador.put ("Puntuacio", puntuacio.toString())
+            var database: FirebaseDatabase = FirebaseDatabase.getInstance("https://mymemory-f114e-default-rtdb.europe-west1.firebasedatabase.app/")
+            var reference: DatabaseReference = database.getReference("DATABASE PLAYERS")
+            if(reference != null){
+                reference.child(uidString).setValue(dadesJugador)
+                Toast.makeText(this, "User Registered",Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "ErrorBD", Toast.LENGTH_SHORT).show()
+            }
+            finish()
+
+        }else{
+            Toast.makeText(this,"error create user :(",Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 
 
